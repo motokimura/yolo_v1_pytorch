cf687562ee6f127f7efbb64e5cfe84564c01d37d
Motoki Kimura <motoki.kimura.1990@gmail.com>
2019-02-01 23:13:18 +0900

    Fix a syntax error of run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,7 @@
 IMAGE="yolo-v1:latest"
 if [ $# -eq 1 ]; then
     IMAGE=$1
+fi
 
 # Set project root dicrectory to map to docker 
 THIS_DIR=$(cd $(dirname $0); pwd)
@@ -11,4 +12,10 @@ PROJ_DIR=`dirname ${THIS_DIR}`
 
 # Run container
 CONTAINER="yolo-v1"
-nvidia-docker run -it --rm --ipc=host -p 6006:6006 -v ${PROJ_DIR}:/work --name ${CONTAINER} ${IMAGE}
+
+nvidia-docker run -it --rm --ipc=host \
+	-p 6006:6006 \
+	-v ${PROJ_DIR}:/workspace \
+	--name ${CONTAINER} \
+	${IMAGE}
+
