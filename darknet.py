b0df6dcf8c8dcd167ed7a9a2f3918704b1baf140
Motoki Kimura <motoki.kimura.1990@gmail.com>
2019-02-02 12:53:15 +0900

    Try to improve training

--- darknet.py
+++ darknet.py
@@ -6,14 +6,65 @@ from util_layers import Squeeze
 
 
 class DarkNet(nn.Module):
-    def __init__(self, conv_only=False):
+    def __init__(self)
         super(DarkNet, self).__init__()
 
-        self.features = self._make_conv_layers()
-        if not conv_only:
-            self.fc = self._make_fc_layers()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 7, stride=2, padding=3),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
 
-        self.conv_only = conv_only
+            nn.Conv2d(64, 192, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(192, 128, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 256, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(512, 256, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 512, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(1024, 512, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 512, 1),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.LeakyReLU(0.1, inplace=True)
+        )
+
+        self.fc = nn.Sequential(
+            nn.AvgPool2d(7),
+            Squeeze(),
+            nn.Linear(1024, 1000)
+        )
 
     def _make_conv_layers(self):
         conv = nn.Sequential(
@@ -78,6 +129,5 @@ class DarkNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        if not self.conv_only:
-            x = self.fc(x)
+        x = self.fc(x)
         return x
