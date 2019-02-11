Fix a bug of loss computation

loss.py
@@ -103,7 +103,7 @@ class Loss(nn.Module):
             noobj_conf_mask[:, 4 + b*5] = 1 # noobj_conf_mask[:, 4] = 1; noobj_conf_mask[:, 9] = 1
         noobj_pred_conf = noobj_pred[noobj_conf_mask]       # [n_noobj, 2=len([conf1, conf2])]
         noobj_target_conf = noobj_target[noobj_conf_mask]   # [n_noobj, 2=len([conf1, conf2])]
-        loss_noobj = F.mse_loss(noobj_pred_conf, noobj_target_conf, reduction='sum')
+        loss_noobj = F.mse_loss(noobj_pred_conf, noobj_target_conf, reduction='mean')
 
         # Compute loss for the cells with objects.
         coord_response_mask = torch.cuda.ByteTensor(bbox_target.size()).fill_(0)    # [n_coord x B, 5]
@@ -143,12 +143,12 @@ class Loss(nn.Module):
         bbox_pred_response = bbox_pred[coord_response_mask].view(-1, 5)      # [n_response, 5]
         bbox_target_response = bbox_target[coord_response_mask].view(-1, 5)  # [n_response, 5], only the first 4=(x, y, w, h) are used
         target_iou = bbox_target_iou[coord_response_mask].view(-1, 5)        # [n_response, 5], only the last 1=(conf,) is used
-        loss_xy = F.mse_loss(bbox_pred_response[:, :2], bbox_target_response[:, :2], reduction='sum')
-        loss_wh = F.mse_loss(torch.sqrt(bbox_pred_response[:, 2:4]), torch.sqrt(bbox_target_response[:, 2:4]), reduction='sum')
-        loss_obj = F.mse_loss(bbox_pred_response[:, 4], target_iou[:, 4], reduction='sum')
+        loss_xy = F.mse_loss(bbox_pred_response[:, :2], bbox_target_response[:, :2], reduction='mean')
+        loss_wh = F.mse_loss(torch.sqrt(bbox_pred_response[:, 2:4]), torch.sqrt(bbox_target_response[:, 2:4]), reduction='mean')
+        loss_obj = F.mse_loss(bbox_pred_response[:, 4], target_iou[:, 4], reduction='mean')
 
         # Class probability loss for the cells which contain objects.
-        loss_class = F.mse_loss(class_pred, class_target, reduction='sum')
+        loss_class = F.mse_loss(class_pred, class_target, reduction='mean')
 
         # Total loss
         loss = self.lambda_coord * (loss_xy + loss_wh) + loss_obj + self.lambda_noobj * loss_noobj + loss_class

train_yolo.py
@@ -25,7 +25,7 @@ val_label = 'data/voc2007test.txt'
 checkpoint_path = 'models/ckpt_darknet_bn.pth.tar'
 
 # Hyper parameters.
-initial_lr = 1.0e-5
+initial_lr = 1.0e-3
 momentum = 0.9
 weight_decay = 5.0e-4
 num_epochs = 135
@@ -36,14 +36,8 @@ def get_lr(epoch, current_lr):
     if epoch == 0:
         lr = initial_lr
     elif epoch == 1:
-        lr = 0.0001
-    elif epoch == 2:
-        lr = 0.0005
-    elif epoch == 3:
-        lr = 0.001
-    elif epoch == 4:
         lr = 0.005
-    elif epoch == 5:
+    elif epoch == 2:
         lr = 0.01
     elif epoch == 75:
         lr = 0.001
@@ -109,7 +103,6 @@ for epoch in range(num_epochs):
     total_loss = 0.0
 
     for i, (imgs, targets) in enumerate(train_loader):
-        batch_size_this_iter = imgs.size(0)
         imgs = Variable(imgs)
         targets = Variable(targets)
         if use_gpu:
@@ -117,7 +110,7 @@ for epoch in range(num_epochs):
 
         preds = yolo(imgs)
         loss = criterion(preds, targets)
-        loss_this_iter = loss.item() / float(batch_size_this_iter)
+        loss_this_iter = loss.item()
         total_loss += loss_this_iter
 
         optimizer.zero_grad()
@@ -132,7 +125,6 @@ for epoch in range(num_epochs):
     val_loss = 0.0
 
     for i, (imgs, targets) in enumerate(val_loader):
-        batch_size_this_iter = imgs.size(0)
         imgs = Variable(imgs)
         targets = Variable(targets)
         if use_gpu:
@@ -141,7 +133,7 @@ for epoch in range(num_epochs):
         with torch.no_grad():
             preds = yolo(imgs)
         loss = criterion(preds, targets)
-        loss_this_iter = loss.item() / float(batch_size_this_iter)
+        loss_this_iter = loss.item()
         val_loss += loss_this_iter
     val_loss /= float(len(val_loader))
 